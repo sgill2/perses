5c5705087044a1e0238827534ca30a2ec0e17635
John Chodera <choderaj@mskcc.org>
2016-12-03 16:35:00 -0500

    Add NCMC plotting analysis that can properly handle multiple environments

--- perses/analysis/analysis.py
+++ perses/analysis/analysis.py
@@ -147,6 +147,98 @@ class Analysis(object):
             plt.close()
 
 
+    def plot_ncmc_work_all_environments(self, filename):
+        """Generate plots of NCMC work for all environments
+
+        Parameters
+        ----------
+        filename : str
+            File to write PDF of NCMC work plots to.
+
+        """
+        with PdfPages(filename) as pdf:
+            for envname in self.get_environments():
+                def plot_work_trajectories(pdf, work, title=""):
+                    """Generate figures for the specified switching legs.
+                    """
+                    plt.figure(figsize=(12, 8))
+
+                    nrows = len(work.keys())
+                    ncols = 6
+                    workcols = 2
+                    for (row, direction) in enumerate(work.keys()):
+                        #
+                        # Plot work vs step
+                        #
+
+                        col = 0
+                        plt.subplot2grid((nrows,ncols), (row, col), colspan=(ncols-workcols))
+
+                        # Plot average work distribution in think solid line
+                        plt.plot(work[direction].mean(0), 'k-', linewidth=1.0, alpha=1.0)
+                        # Plot bundle of work trajectories in transparent lines
+                        plt.plot(work[direction].T, 'k-', linewidth=0.5, alpha=0.3)
+                        # Adjust axes to eliminate large-magnitude outliers (keep 98% of data in-range)
+                        workvals = np.ravel(np.abs(work[direction]))
+                        worklim = np.percentile(workvals, 98)
+                        nsteps = work[direction].shape[1]
+                        plt.axis([0, nsteps, -worklim, +worklim])
+                        # Label plot
+                        if row == 1: plt.xlabel('steps')
+                        plt.ylabel('work / kT')
+                        plt.title("%s NCMC in environment '%s' : %s" % (title, envname, direction))
+                        plt.legend(['average work', 'NCMC attempts'])
+
+                        #
+                        # Plot work histogram
+                        #
+
+                        col = ncols - workcols
+                        plt.subplot2grid((nrows,ncols), (row, col), colspan=workcols)
+
+                        # Plot average work distribution in think solid line
+                        #nbins = 40
+                        workvals = work[direction][:-1,-1]
+                        #plt.hist(workvals, nbins)
+                        if workvals.std() != 0.0:
+                            sns.distplot(workvals, rug=True)
+                        else:
+                            print('workvals has stddev of zero')
+                            print(workvals)
+                        # Adjust axes to eliminate large-magnitude outliers (keep 98% of data in-range)
+                        #worklim = np.percentile(workvals, 98)
+                        #oldaxis = plt.axis()
+                        #plt.axis([-worklim, +worklim, 0, oldaxis[3]])
+                        # Label plot
+                        if row == 1: plt.xlabel('work / kT')
+                        plt.title("total %s work" % direction)
+
+                    pdf.savefig()  # saves the current figure into a pdf page
+                    plt.close()
+
+                for modname in ['NCMCEngine', 'NCMCHybridEngine']:
+                    work = dict()
+                    for direction in ['delete', 'insert']:
+                        varname = '/' + envname + '/' + modname + '/' + 'work_' + direction
+                        try:
+                            # TODO: For now, we analyze all but the last sample, so that this can be run on active simulations.
+                            # Later, we should find some way to omit the last sample only if it is nonsensical.
+                            work[direction] = self._ncfile[varname][:-1,:]
+                            print('Found %s' % varname)
+                        except Exception as e:
+                            pass
+                    
+                    if len(work) > 0:
+                        # Plot work for all chemical transformations.
+                        plot_work_trajectories(pdf, work, title='(all transformations)')
+
+                        # Plot work separated out for each chemical transformation
+                        #[niterations, nsteps] = work.shape
+                        #transformations = dict()
+                        #for iteration in range(niterations):
+                        #    plot_work_trajectories(pdf, work, title='(all transformations)')
+
+
     def plot_ncmc_work(self, filename):
         """Generate plots of NCMC work.
 
@@ -157,7 +249,7 @@ class Analysis(object):
 
         """
         with PdfPages(filename) as pdf:
-            for envname in ['NCMCEngine', 'NCMCHybridEngine']: #self.get_environments():
+            for envname in ['NCMCEngine', 'NCMCHybridEngine']:
                 modname = envname
                 work = dict()
                 for direction in ['delete', 'insert']:
